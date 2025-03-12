bdae660448e3caa7b80fe076aa1ba2816e3b06a1
IyXuan23 <127928542+IyXuan23@users.noreply.github.com>
2025-03-12 11:37:36 +0800

    Added new 'game' route

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue'
 import AboutPage from './components/AboutPage.vue'
+import GamePage from './components/GamePage.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,6 +16,11 @@ const router = createRouter({
             path: '/about',
             name: 'about',
             component: AboutPage
+        },
+        {
+            path: '/game',
+            name: 'game',
+            component: GamePage
         }
     ]
 })
